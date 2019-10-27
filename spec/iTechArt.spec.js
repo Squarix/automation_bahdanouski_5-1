c97bc883da617686c4344e47a951f764cc298c2f
uladzislau bahadanouski <uladzislau.b@nyblecraft.com>
2019-10-28 01:29:58 +0300

    Changed logic of beforeAll and beforeEach

--- spec/iTechArt.spec.js
+++ spec/iTechArt.spec.js
@@ -1,5 +1,5 @@
-const {Builder, By, Key, until, WebElement, WebDriverWait, ExpectedConditions} = require('selenium-webdriver');
-const {browser, searchResults} = require('./testData');
+const {Builder, By} = require('selenium-webdriver');
+const {browser, searchResults, url, query} = require('./testData');
 const config = require('./config');
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = config.timeout;
@@ -10,14 +10,9 @@ function findText(search, entry) {
 
 async function checkElements(elements, search) {
 	await Promise.all(elements.map(async element => {
-		const urlElement = await element.findElement(By.tagName('cite'));
-		const url = await urlElement.getText();
-
-		const headerElement = await element.findElement(By.tagName('h3'));
-		const header = await headerElement.getText();
-
-		const descriptionElement = await element.findElement(By.className('st'));
-		const description = await descriptionElement.getText();
+		const url = await element.findElement(By.tagName('cite')).getText();
+		const header = await element.findElement(By.tagName('h3')).getText();
+		const description = await element.findElement(By.className('st')).getText();
 
 		const urlContains = findText(search, url);
 		const descContains = findText(search, description);
@@ -28,26 +23,34 @@ async function checkElements(elements, search) {
 }
 
 describe('Google ', () => {
-	beforeEach(async () => {
+	beforeAll(async () => {
 		this.driver = new Builder()
 			.forBrowser(browser)
 			.build();
 
-		this.iTechArt = 'iTechArt'.toUpperCase();
+		this.query = query.toUpperCase();
+	});
+
+	beforeEach(async () => {
 		await doSearch();
 	});
 
 	it ('Check results amount', async () => {
 		const resultsCount = this.driver.findElement(By.id('resultStats'));
+		const resultsText = await resultsCount.getText();
+
+		console.log(resultsText);
+
 		const myRegexp = /примерно ([\s\S]*?) \(/;
-		const match = myRegexp.exec(await resultsCount.getText())[1];
+		const match = myRegexp.exec(resultsText)[1];
 		const total = Number.parseInt(match.replace(' ', ''));
+
 		expect(total > searchResults).toBe(true);
 	});
 
 	it('Should check all results on first page', async () => {
 		const firstPage = await this.driver.findElements(By.className('rc'));
-		await checkElements(firstPage, this.iTechArt);
+		await checkElements(firstPage, this.query);
 	});
 
 	it('Should check all result on second page', async () => {
@@ -55,27 +58,28 @@ describe('Google ', () => {
 		await secondPageButton.click();
 
 		const secondPage = await this.driver.findElements(By.className('rc'));
-		await checkElements(secondPage, this.iTechArt);
+		await checkElements(secondPage, this.query);
 	});
 
-	afterEach(async () => {
-		await this.driver.quit();
-	});
 
 	function timeout(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
 
 	const doSearch = async () => {
-		await this.driver.get('https://www.google.com');
+		await this.driver.get(url);
 		const element = this.driver.findElement(By.name('q'));
-		await element.sendKeys('ITechArt');
+		await element.sendKeys(query);
 
 		const submit = this.driver.findElement(By.xpath('.//form//input[contains(@aria-label, \'Поиск в Google\')]'));
 
 		// waiting for autocomplete
 		await timeout(1000);
 		await submit.click();
-	}
+	};
+
+	afterAll(async () => {
+		await this.driver.quit();
+	});
 
 });
--- spec/testData.js
+++ spec/testData.js
@@ -1,4 +1,6 @@
 module.exports = {
 	searchResults: 50000,
-	browser: 'chrome'
+	browser: 'chrome',
+	query: 'iTechArt',
+	url: 'https://google.com',
 }
