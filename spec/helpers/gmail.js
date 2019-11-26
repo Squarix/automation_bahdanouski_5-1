da11bcdc0df42323168d650bd7348e4e64123bc7
uladzislau bahadanouski <uladzislau.b@nyblecraft.com>
2019-11-26 14:12:41 +0300

    token

--- spec/helpers/gmail.js
+++ spec/helpers/gmail.js
@@ -1,9 +1,13 @@
 const fs = require('fs').promises;
+
 const {google} = require('googleapis');
+
 const base64 = require('js-base64').Base64;
 const logger = require('./../helpers/logger');
+const readline = require('readline');
 
-const CREDENTIALS_PATH = 'credentials.json';
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const CREDENTIALS_PATH = 'credentials3.json';
 const TOKEN_PATH = 'token.json';
 
 let countMessages = 0;
@@ -11,11 +15,12 @@ let countMessages = 0;
 module.exports.authorize = async function () {
 	const credentials = await fs.readFile(CREDENTIALS_PATH);
 	const {client_secret, client_id, redirect_uris} = JSON.parse(credentials).installed;
-	const oAuth2Client = new google.auth.OAuth2(
+	let oAuth2Client = new google.auth.OAuth2(
 		client_id, client_secret, redirect_uris[0]);
 
-	let token = await fs.readFile(TOKEN_PATH);
-	oAuth2Client.setCredentials(JSON.parse(token));
+	oAuth2Client = await getNewToken(oAuth2Client);
+	//let token = await fs.readFile(TOKEN_PATH);
+	//oAuth2Client.setCredentials(JSON.parse(token.toString()));
 	return oAuth2Client;
 };
 
@@ -34,7 +39,6 @@ module.exports.countMessages = async function (client) {
 async function getMessages(client) {
 	logger.debug(`Start getting messages`);
 	const gmail = google.gmail({version: 'v1', client});
-	console.log(client);
 	let messages = await gmail.users.messages.list({userId: 'me'});
 	logger.debug(messages.data);
 	logger.debug(`End getting messages`);
@@ -61,6 +65,30 @@ async function processMessage(id, auth) {
 		logger.warn('Subject is inappropriate');
 	}
 	logger.debug('End process message');
+}
+
+async function getNewToken(oAuth2Client) {
+	return new Promise(resolve => {
+		const authUrl = oAuth2Client.generateAuthUrl({
+			access_type: 'offline',
+			scope: SCOPES,
+		});
+		console.log('Authorize this app by visiting this url:', authUrl);
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+		rl.question('Enter the code from that page here: ', (code) => {
+			rl.close();
+			oAuth2Client.getToken(code, async (err, token) => {
+				if (err) return console.error('Error retrieving access token', err);
+				oAuth2Client.setCredentials(token);
+				// Store the token to disk for later program executions
+				await fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+				resolve(oAuth2Client)
+			});
+		});
+	})
 }
 
 function getSubject(messageData) {
